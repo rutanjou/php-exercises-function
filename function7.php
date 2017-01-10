48314afa3083f4280341ade9c170605dcd780d54
Ru Tan <jutan2012@gmail.com>
2017-01-10 10:05:46 +0100

    Ajout html form label input

---
+++ function7.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exer 7</title>
+</head>
+<body>
+<form method="post" action="func7.php">
+	<select name="genre">
+		<option value="homme">Homme</option>
+		<option value="femme">Femme</option>
+	</select>
+	<label for="age">Age: </label>
+	<input type="number" name="age" id="ageNumber">
+	<input type="submit" value="Envoyer">
+	
+	
+</form>
+</body>
+</html>
